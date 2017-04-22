d8037e2fae589d69f5505897db8b394e2ecaf941
laaph <laaph@laaph.com>
2017-04-22 15:47:20 -0700

    added some

--- Assets/Scripts/FauxGravityAttractor.cs
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -9,7 +9,6 @@ public class FauxGravityAttractor : MonoBehaviour {
 
 	public float targetRotation;
 
-	public List<GameObject> objects = new List<GameObject>();
 
 	public void Attract(Transform body, Rigidbody2D rb) {
 		Vector2 gravityUp = (body.position - transform.position).normalized;
@@ -19,13 +18,10 @@ public class FauxGravityAttractor : MonoBehaviour {
 
 		//Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp);
 
-		targetRotation = -Mathf.Atan2(body.position.y, body.position.x) * Mathf.Rad2Deg + 90f; // For other objects
+		targetRotation = -Mathf.Atan2(body.position.y, body.position.x) * Mathf.Rad2Deg - 90f; // For other objects
 		//body.rotation = Quaternion.Slerp(body.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-		//body.rotation = Quaternion.Euler( new Vector3(0f, 0f, targetRotation));
-		//body.Rotate(new Vector3(0f, 0f, targetRotation));
-		foreach( GameObject g in objects) {
-			g.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, targetRotation));
-		}
+		body.rotation = Quaternion.Euler( new Vector3(0f, 0f, targetRotation));
+		body.Rotate(new Vector3(0f, 0f, targetRotation));
 	}
 }
