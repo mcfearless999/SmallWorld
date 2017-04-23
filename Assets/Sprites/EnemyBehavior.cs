41ba5e048d11ca51b996fdaa11d0dd4aec26e1c4
Chris M <chris@techfocus.net>
2017-04-22 22:29:42 -0700

    added monster script

--- Assets/Sprites/EnemyBehavior.cs
+++ Assets/Sprites/EnemyBehavior.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyBehavior : MonoBehaviour {
 
+	public bool facingLeft = true;
+	float speed = 10.0f;
+	float origX;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		//Vector3 origPosition = transform.position;
+		origX = transform.position.x;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		transform.Translate(speed*Time.deltaTime,0,0);
+
+		if(Mathf.Abs(origX - transform.position.x) > 4.0f)
+		{
+			
+			speed *= -1.0f; //change direction
+			Flip(); 
+		}
 	}
+	public void Flip() {
+		facingLeft = !facingLeft;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
 }
